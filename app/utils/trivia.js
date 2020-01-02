b2fa8eea7f8926d181a9470513d4a947dce040d5
Erin Singer <erinsinger93@gmail.com>
2020-01-02 09:36:23 -0800

    New Years 2019 trivia questions

--- app/utils/trivia.js
+++ app/utils/trivia.js
@@ -1,91 +1,95 @@
+function getAnswerHTML(answer) {
+  return `<span class="trivia__answer">${answer}</span>`;
+}
+
+function getAnswerLabelHTML(label) {
+  return `<span class="trivia__answer-label">${label}:</span>`;
+}
+
+function getSongAnswerHTML(song, artist) {
+  return `${getAnswerLabelHTML('Song')}
+          ${getAnswerHTML(song)} <br/>
+          ${getAnswerLabelHTML('Artist')}
+          ${getAnswerHTML(artist)}`;
+}
+
+function getMovieAnswerHtml(movie, actor, name) {
+  return `${getAnswerLabelHTML('Movie')}
+          ${getAnswerHTML(movie)} <br/>
+          ${getAnswerLabelHTML('Actor')}
+          ${getAnswerHTML(actor)} <br/>
+          ${getAnswerLabelHTML('Character Name')}
+          ${getAnswerHTML(name)}`;
+}
+
 const Trivia = [
   {
     question: 'Name the song and the artist',
-    audio: 'assets/audio/TimMcGraw.m4a',
-    answer: 'Artist: Tim McGraw, Song: I Like It, I Love It',
+    audio: 'assets/2019/audio/need-you-now.mp3',
+    answer: getSongAnswerHTML('Need You Now', 'Lady Antebellum'),
     type: 'song',
     pointValue: 2
   }, {
     question: 'Name the song and the artist',
-    audio: 'assets/audio/Hoobastank.m4a',
-    answer: 'Artist: Hoobastank, Song: The Reason',
+    audio: 'assets/2019/audio/how-to-save-a-life.mp3',
+    answer: getSongAnswerHTML('How to Save a Life', 'The Fray'),
     type: 'song',
     pointValue: 2
   }, {
     question: 'Name the song and the artist',
-    audio: 'assets/audio/GeorgeHarrison.m4a',
-    answer: 'Artist: George Harrison, Song: My Sweet Lord',
+    audio: 'assets/2019/audio/glad-you-came.mp3',
+    answer: getSongAnswerHTML('Glad You Came', 'The Wanted'),
     type: 'song',
     pointValue: 2
   }, {
     question: 'Name the song and the artist',
-    answer: 'Artist: A-ha, Song: Take On Me',
-    audio: 'assets/audio/aHa.m4a',
+    audio: 'assets/2019/audio/ramblin-man.mp3',
+    answer: getSongAnswerHTML('Ramblin\' Man', 'The Allman Brothers Band'),
     type: 'song',
     pointValue: 2
   }, {
-    question: 'Name the movie title, character name, and actor',
-    answer: 'Title: Dumb and Dumber, Character: Lloyd Christmas, Actor: Jim Carrey',
-    audio: 'assets/audio/dumbAndDumber.m4a',
+    question: 'Name the movie title, actor, and character name',
+    image: 'assets/2019/images/Easy-A-Emma-Stone-Olive.png',
+    answer: getMovieAnswerHtml('Easy A', 'Emma Stone', 'Olive'),
     type: 'movie',
     pointValue: 3
   }, {
-    question: 'Name the movie title, character name, and actor',
-    audio: 'assets/audio/homeAlone.m4a',
-    answer: 'Title: Home Alone, Character: Kevin McCallister, Actor: Macaulay Culkin',
+    question: 'Name the movie title, actor, and character name',
+    image: 'assets/2019/images/Pulp-Fiction-Uma-Thurman-Mia.png',
+    answer: getMovieAnswerHtml('Pulp Fiction', 'Uma Thurman', 'Mia'),
     type: 'movie',
     pointValue: 3
   }, {
-    question: 'Name the movie title, character name, and actor',
-    audio: 'assets/audio/theGoodTheBadAndTheUgly.m4a',
-    answer: 'Title: The Good, the Bad and the Ugly, Character: Blondie, Actor: Clint Eastwood',
+    question: 'Name the movie title, actor, and character name',
+    image: 'assets/2019/images/Houdini-1953-Tony-Curtis-Harry-Houdini.png',
+    answer: getMovieAnswerHtml('Houdini', 'Tony Curtis', 'Harry Houdini'),
     type: 'movie',
     pointValue: 3
   }, {
-    question: 'Name the movie title, both character names, and both actors',
-    audio: 'assets/audio/findingNemo.m4a',
-    answer: 'Title: Finding Nemo, Characters: Dory & Marlin, Actors: Ellen DeGeneres & Albert Brooks',
+    question: 'Name the movie title, actor, and character name',
+    image: 'assets/2019/images/Oceans-Eleven-Brad-Pitt-Rusty-Ryan.jpg',
+    answer: getMovieAnswerHtml('Oceans Eleven', 'Brad Pitt', 'Rusty Ryan'),
     type: 'movie',
-    pointValue: 5
+    pointValue: 3
   }, {
-    question: 'What 90s toy became the one of the most popular toys of 2017?',
-    answer: 'The Fidget Spinner',
-    type: 'pop-culture',
+    question: `${getAnswerLabelHTML('Fact or fiction')}<br/>The California flag was the result of a mistake; it was supposed to feature a pear.`,
+    answer: 'Fiction. This was perpetuated by an internet hoax that was quickly debunked.',
+    type: 'trivia',
     pointValue: 1
   }, {
-    question: 'Which celebrity was the executive producer for the TV show 13 Reasons Why?',
-    answer: 'Selena Gomez',
-    type: 'pop-culture',
-    pointValue: 1
-  }, {
-    question: 'The most successful film in Golden Globe Awards history, what film won all seven awards for which it had been nominated?',
-    answer: 'La La Land',
-    type: 'pop-culture',
+    question: 'Which planet is the hottest planet in our solar system?',
+    answer: 'Venus, which reaches up to 870°F',
+    type: 'trivia',
     pointValue: 2
   }, {
-    question: 'Hollywood actor Jake has starred in movies such as Brokeback Mountain, Southpaw, and Nightcrawler. But how do you spell his last name?',
-    answer: 'Jake Gyllenhaal',
-    type: 'pop-culture',
-    pointValue: 2
-  }, {
-    question: 'Who is the only athlete ever to play in a Super Bowl and a World Series?',
-    answer: 'Deion Sanders',
-    type: 'sports',
-    pointValue: 1
-  }, {
-    question: 'What Boston Red Sox player has the most career hits?',
-    answer: 'Carl Yastrzemski with 3,419 career hits',
-    type: 'sports',
-    pointValue: 2
-  }, {
-    question: 'Which is the only country to have played in every World Cup?',
-    answer: 'Brazil',
-    type: 'sports',
-    pointValue: 2
-  }, {
-    question: 'Who hit an underhand serve to defat Ivan Lendl for the 1989 French Open title? Jim Courier, Michael Chang, Björn Borg, or Mats Wilander?',
+    question: 'Who beat Ivan Lendl in the French Open finals serving underhand?',
     answer: 'Michael Chang',
-    type: 'sports',
+    type: 'trivia',
+    pointValue: 2
+  }, {
+    question: 'Which company owns Bugatti, Lamborghini, Audi, Porsche, and Ducati?',
+    answer: 'Volkswagen',
+    type: 'trivia',
     pointValue: 2
   }
 ];
